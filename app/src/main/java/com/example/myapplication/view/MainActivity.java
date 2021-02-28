4d75bb4f7eb84bccf295470638dda0483c7c8bda
Brenda Paetzoldt Silva <brendapaetzoldt@yahoo.com.br>
2021-02-28 16:01:14 -0300

    ActivityMain e Tela de cadastro de livros feita

--- app/src/main/java/com/example/myapplication/view/MainActivity.java
+++ app/src/main/java/com/example/myapplication/view/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.myapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activity_cadastro.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
