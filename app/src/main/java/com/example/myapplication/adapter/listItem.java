d60487058d8f6e562a713cdab478ef48f0367f82
Brenda Paetzoldt Silva <brendapaetzoldt@yahoo.com.br>
2021-04-04 02:33:30 -0300

    listview adapter

---
+++ app/src/main/java/com/example/myapplication/adapter/listItem.java
@@ -0,0 +1,50 @@
+package com.example.myapplication.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.myapplication.R;
+import com.example.myapplication.model.Livro;
+
+import java.util.List;
+
+public class listItem extends ArrayAdapter<Livro> {
+
+    private Context context;
+    private List<Livro> livros;
+
+    public listItem(@NonNull Context context, int resource, @NonNull List<Livro> livros) {
+        super(context, resource, livros);
+        this.context = context;
+        this.livros = livros;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Livro livro = livros.get(position);
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+
+
+        convertView = inflater.inflate(R.layout.list_item, parent, false);
+
+
+        TextView txt_autor = convertView.findViewById(R.id.txt_autor);
+        TextView txt_titulo = convertView.findViewById(R.id.txt_titulo);
+
+
+        txt_autor.setText(livro.getTitulo());
+        txt_titulo.setText(livro.getAutor());
+
+
+        return convertView;
+    }
+}
